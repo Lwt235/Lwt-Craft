8cb9ba0a886860777a0a221f545815df3e2c618f
Lwt235 <m15955295202@163.com>
2023-11-28 21:37:15 +0800

    修改部分配置

--- mytest/src/App.vue
+++ mytest/src/App.vue
@@ -27,10 +27,10 @@
             <router-view></router-view>
         </el-main>
     </el-container>
-    <a href="https://beian.mps.gov.cn/#/query/webSearch?code=43010402001517" rel="noreferrer" target="_blank" style="text-decoration: none; color:black">湘公网安备43010402001517</a>
-    <br>
-    <img src="./assets/beian.png" width="16" height="17.28">
     <a href="https://beian.miit.gov.cn/" target="_blank" style="text-decoration: none; color:black">湘ICP备2023030377号-1</a>
+    <br>
+    <img src="./assets/beian.png" width="16" height="17.28" style="vertical-align: middle;">
+    <a href="https://beian.mps.gov.cn/#/query/webSearch?code=43010402001517" rel="noreferrer" target="_blank" style="text-decoration: none; color:black">湘公网安备43010402001517</a>
 </template>
 
 <script setup>
--- mytest/vue.config.js
+++ mytest/vue.config.js
@@ -15,3 +15,12 @@ module.exports = {
     },
   },
 }
+module.exports = {
+  //webpack配置
+configureWebpack: {
+    //关闭 webpack 的性能提示
+    performance: {
+      hints:false
+    }
+  }
+}
